e02da5656d3495341eb22d43f2b6c57443d8edaa
zwy <15133687879@163.com>
2024-11-19 23:35:08 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-qemu-system-x86_64 -blockdev driver=file,node-name=f0,filename=./build/imgs/boot.img -device floppy,drive=f0
+qemu-system-i386 \
+    -blockdev driver=file,node-name=hdd,filename=./build/imgs/boot.img \
+    -device ide-hd,drive=hdd \
+    -s -S
